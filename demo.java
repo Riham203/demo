884c5face713b7c9be2d80c9ce01e7f5a4671a6b
Riham203 <137186655+Riham203@users.noreply.github.com>
2025-02-12 12:03:35 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,10 +1,6 @@
 class demo{
   public static void main(String [] args)
   {
-    int a = 10;
-    int b =20;
-    int sum = a+b;
-    System.out.println(sum);
+    System.out.println("DEMO");
   }
-}
 }
